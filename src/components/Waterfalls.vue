2dcf98128d7ba29001481f21137adfbb25692186
zhuhuoxingguang <zhuhuoxingguang@sina.cn>
2018-07-06 13:33:38 +0800

    修正一次滑动，多次请求的问题

--- src/components/Waterfalls.vue
+++ src/components/Waterfalls.vue
@@ -3,7 +3,7 @@
    class="main"
     :on-refresh="refresh"
     :on-infinite="infinite">
-    <div class="waterfalls">
+      <div class="waterfalls">
         <ul>
           <li class="photo" v-for="item in waterfallsLeft" :key="item.id + item.albumId+Math.random()">
             <a href="javascrip:;">
@@ -32,7 +32,7 @@
               <img :src="item.src" alt="">
             </a>
             <div class="desc-info">
-              <p>编号：<span>00001</span></p>
+              <p>编号：<span>{{item.albumId}}</span></p>
             </div>
             <div class="thumbnail">
               <div class="thumbnail-desc">
@@ -73,55 +73,66 @@ export default {
     refresh(done){
       window.location.reload();
     },
+    
     infinite(done){
-      this.getData(done);
+      this.$nextTick(function () { 
+        this.getData(done);
+      })
     },
     getData(done){
-      console.log(this.page)
       let _this = this;
       axios.get("https://jsonplaceholder.typicode.com/photos", {page: this.page})
           .then(res=>res.data)
           .then(res => {
+            console.log(this.page)
             let counts = res.slice((this.page-1) * this.pageCount, this.page * this.pageCount)
-            //  console.log(counts.length)
-            counts.forEach(item => {
+            counts.forEach((item, index) => {
               item.src ='http://cued.xunlei.com/demos/publ/img/P_'+ this.randomNum()+ '.jpg'
             });
             this.page++;
             this.itemsLen = counts.length;
-            this.judgeAllLoaded(counts)
-            self.bottom = self.bottom + 10;
-            if(done) done();
+            this.judgeAllLoaded(counts, done)
+            
           })
     },
     randomNum(){  // 三位数随机数，162以内
       let random = Math.floor(Math.random() * 162)
       return random = random < 10 ? '00'+random : random < 100 ? '0'+random : ''+random;
     },
-    judgeAllLoaded(items){   // 判断所有图片是否加载完成
+    judgeAllLoaded(items, done){   // 判断所有图片是否加载完成
       let _this = this;
-      items.forEach(item => {
-        let IMG = new Image();
-        IMG.src = item.src;
-        IMG.width = 100;
-        IMG.onload = function () {  
-          _this.flag++;
-          if(_this.leftHeight <= _this.rightHeight){
-            _this.leftHeight += IMG.height;
-            _this.waterfallsLeft.push(item)
-          }else{
-            _this.rightHeight += IMG.height;
-            _this.waterfallsRight.push(item)
+      this.$nextTick(function(){
+        items.forEach(item => {
+          let IMG = new Image();
+          IMG.src = item.src;
+          IMG.width = 100;
+          IMG.onload = function () {  
+            _this.flag++;
+            if(_this.flag == _this.itemsLen){
+              // 这段是标志位，标识这一次的滑动请求完成
+              // 之前的这一段是直接放在异步请求函数里面的，产生的结果就是在某些时候，一次滑动会发送一次或者一次以上请求
+              // 由于图片加载有异步过程，在所有图片加载完成之后，再标识本次请求结束，避免出现多次请求
+              this.bottom = this.bottom + 10;
+              if(done) done();
+            }
+            // 滑动加载之后，恢复标识图片加载完成的数据
+            if(_this.flag > _this.itemsLen){
+              _this.flag = 1;
+            }
+            if(_this.leftHeight <= _this.rightHeight){
+              _this.leftHeight += IMG.height;
+              _this.waterfallsLeft.push(item)
+            }else{
+              _this.rightHeight += IMG.height;
+              _this.waterfallsRight.push(item)
+            }
           }
-        }
+        })
       })
     }
-  },
+  }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-
 <style lang="scss" scoped>
 .main{
   padding: 5px 10px;
@@ -148,7 +159,6 @@ export default {
     display: inline-block;
     img{
       width: 100%;
-
     }
   }
   .desc-info{
